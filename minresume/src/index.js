208a7288cbc08560bc44e0df215a554605cebe10
NaN <2513784586@qq.com>
2018-05-09 01:10:22 +0800

    仿

---
+++ minresume/src/index.js
@@ -0,0 +1,8 @@
+/**
+ * Created by acer on 2018/4/17.
+ */
+var banner = document.getElementsByClassName('banner');
+var ba = document.getElementById('ba-na');
+ba.onMouseOver = function starAnimation(){
+        var starTime = Date.now();
+}
