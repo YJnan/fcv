a8f7b01b0d3aa465df29d832c55da657665c0aaf
NaN <2513784586@qq.com>
2019-06-24 11:27:40 +0800

    更改素材

--- minresume/src/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by acer on 2018/4/17.
- */
-var banner = document.getElementsByClassName('banner');
-var ba = document.getElementById('ba-na');
-ba.onMouseOver = function starAnimation(){
-        var starTime = Date.now();
-}
